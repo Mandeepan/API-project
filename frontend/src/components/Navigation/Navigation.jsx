5f1750b815a9644b436ccc7072e7ed45a82516fe
mandeepan <mandyphc@gmail.com>
2024-10-13 12:39:01 -0400

    login & signup modal setup completed.

--- frontend/src/components/Navigation/Navigation.jsx
+++ frontend/src/components/Navigation/Navigation.jsx
@@ -1,35 +1,48 @@
-// frontend/src/components/Navigation/Navigation.jsx
-import './Navigation.css';
 import { NavLink } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import ProfileButton from './ProfileButton';
+import OpenModalButton from '../OpenModalButton';
+import LoginFormModal from '../LoginFormModal';
+import SignupFormModal from '../SignupFormModal';
+import './Navigation.css';
 
-function Navigation({ isLoaded }){
-    const sessionUser = useSelector(state => state.session.user);
+function Navigation({ isLoaded }) {
+  const sessionUser = useSelector((state) => state.session.user);
 
-    const sessionLinks = sessionUser ? (
-    <li>
+  let sessionLinks;
+  if (sessionUser) {
+    sessionLinks = (
+      <li>
         <ProfileButton user={sessionUser} />
-    </li>
-    ) : (
+      </li>
+    );
+  } else {
+    sessionLinks = (
     <>
         <li>
-        <NavLink to="/login">Log In</NavLink>
-        </li>
-        <li>
-        <NavLink to="/signup">Sign Up</NavLink>
-        </li>
+        <OpenModalButton
+          buttonText="Log In"
+          modalComponent={<LoginFormModal />}
+        />
+      </li>
+      <li>
+        <OpenModalButton
+          buttonText="Sign Up"
+          modalComponent={<SignupFormModal />}
+        />
+      </li>
     </>
+      
     );
-
-    return (
+  }
+  return (
     <ul>
-        <li>
+      <li>
         <NavLink to="/">Home</NavLink>
-        </li>
-        {isLoaded && sessionLinks}
+      </li>
+      {isLoaded && sessionLinks}
     </ul>
-    );
+  );
 }
 
 export default Navigation;
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -8,7 +8,7 @@ import { Provider } from 'react-redux';
 import configureStore from './store';
 import { restoreCSRF, csrfFetch } from './store/csrf';
 import * as sessionActions from './store/session';
-
+import { Modal, ModalProvider } from './context/ModalContext/Modal';
 
 const store = configureStore();
 
@@ -22,8 +22,11 @@ if (import.meta.env.MODE !== 'production') {
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <ModalProvider>
+      <Provider store={store}>
+        <App />
+        <Modal />
+      </Provider>
+    </ModalProvider>
   </React.StrictMode>
 );
