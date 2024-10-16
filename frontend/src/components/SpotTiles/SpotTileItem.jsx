68801ab7caf679df74c4a49760f3855212e9ad40
mandeepan <mandyphc@gmail.com>
2024-10-15 20:35:57 -0400

    adding all spots css style

--- frontend/src/components/SpotTiles/SpotTileItem.jsx
+++ frontend/src/components/SpotTiles/SpotTileItem.jsx
@@ -1,4 +1,5 @@
-import './SpotTiles.css';
+import { FaStar } from "react-icons/fa";
+
 
 export default function SpotTileItem({ spot }) {
     return (
@@ -14,12 +15,15 @@ export default function SpotTileItem({ spot }) {
             <div className="SpotTileItemDescription">
                 <div className="SpotTileItemFirstRow">
                     <div><strong>{spot.city}, {spot.state}</strong></div>
-                    {spot.avgRating && <div className="SpotTileItemStarRating">
-                                            <i className="fa-solid fa-star SpotTileItemStar" /> {spot.avgRating}
-                    </div>}
+                    {spot.avgRating? <div className="SpotTileItemStarRating">
+                                            <FaStar />
+                                            <i className="fa-solid fa-star SpotTileItemStar" /> {spot.avgRating}</div>
+                                    :<div className="SpotTileItemStarRating"><strong>New</strong> </div>
+                    }
                 </div>
-                <div className="SpotTileItemPrice"><strong>${spot.price}</strong> night</div>
+                <div className="SpotTileItemPrice"><strong>$ {spot.price}</strong> night</div>
             </div>
         </div>
+
     );
 }
--- frontend/src/components/SpotTiles/SpotTiles.jsx
+++ frontend/src/components/SpotTiles/SpotTiles.jsx
@@ -1,9 +1,9 @@
-import './SpotTiles.css'
+import './SpotTiles.css';
 import { useDispatch, useSelector } from 'react-redux';
 import { useEffect } from 'react';
 import {getAllSpots} from '../../store/spots';
 import SpotTileItem from './SpotTileItem';
-import * as PageNotFound from '../PageNotFound';
+// import * as PageNotFound from '../PageNotFound';
 
 export default function SpotTiles(){
     const dispatch =useDispatch();
