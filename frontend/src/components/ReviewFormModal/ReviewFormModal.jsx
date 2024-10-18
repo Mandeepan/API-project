895222fc2dbe5e6a856095f254230f18c784f667
mandeepan <mandyphc@gmail.com>
2024-10-18 00:53:34 -0400

    adding useEffect to hook live changes in input fields

--- frontend/src/components/ReviewFormModal/ReviewFormModal.jsx
+++ frontend/src/components/ReviewFormModal/ReviewFormModal.jsx
@@ -1,20 +1,42 @@
 import { useModal } from '../../context/ModalContext/Modal'
 import { FaStar } from 'react-icons/fa';
-import {useState} from 'react'
+import {useEffect, useState} from 'react'
 import './ReviewFormModal.css'
 
-export default function ReviewFormModal (){
+export default function ReviewFormModal ({spotId}){
     const [hoverValue, setHoverValue] = useState(0);
     const [selectedValue, setSelectedValue] = useState(0);
     const [reviewContent, setReviewContent]=useState("")
+    const [errors, setErrors] = useState({});
 
     const handleMouseOver = (value) => {setHoverValue(value);};
     const handleMouseLeave = () => {setHoverValue(0);};
     const handleClick = (value) => {setSelectedValue(value);};
 
     const { closeModal } = useModal()
+
+    useEffect(()=>{
+        const newErrors = {};
+		if (reviewContent && reviewContent.length < 10)
+			newErrors.review = 'Review must be at least 10 characters long';
+		if (hoverValue !==0 && selectedValue < 1 )
+			newErrors.stars = 'Stars must be between 1 and 5';
+
+		if (Object.keys(newErrors).length > 0) {
+			setErrors(newErrors);
+			return;
+		}
+    }, [reviewContent,selectedValue])
+
+    
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+		// await dispatch(addAReview({ review, stars }, spotId));
+		closeModal();
+	};
+
     return (
-    <form className="create-review-form" >
+    <form className="create-review-form" onSubmit={handleSubmit}>
          <h1 className="review-form-title">How was your stay?</h1>
          <input type='text' 
                 className="review-form-input" 
@@ -23,6 +45,7 @@ export default function ReviewFormModal (){
 				onChange={(e) => setReviewContent(e.target.value)}
                 required>
         </input>
+        {errors.review && <p className='error'>{errors.review}</p>}
          <div className='review-star-container'>
             <div className="star-rating">
                     {Array.of(1,2,3,4,5).map((_, index) => {
@@ -41,6 +64,7 @@ export default function ReviewFormModal (){
             </div>
             <label><strong> Stars</strong></label>
          </div>
+         {errors.stars && <p className='error'>{errors.stars}</p>}
          <button className="review-submit-button" onClick={closeModal} disabled={reviewContent.length<10 || !selectedValue}>Submit Your Review</button>
     </form> 
     )
