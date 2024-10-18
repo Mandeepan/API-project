12b69aa2b8c6979cb32c443a898757f253205d4a
mandeepan <mandyphc@gmail.com>
2024-10-18 00:22:55 -0400

    create-review front end info validation completed

--- frontend/src/components/ReviewFormModal/ReviewFormModal.jsx
+++ frontend/src/components/ReviewFormModal/ReviewFormModal.jsx
@@ -6,24 +6,23 @@ import './ReviewFormModal.css'
 export default function ReviewFormModal (){
     const [hoverValue, setHoverValue] = useState(0);
     const [selectedValue, setSelectedValue] = useState(0);
+    const [reviewContent, setReviewContent]=useState("")
 
-    const handleMouseOver = (value) => {
-        setHoverValue(value);
-      };
-    
-      const handleMouseLeave = () => {
-        setHoverValue(0);
-      };
-    
-      const handleClick = (value) => {
-        setSelectedValue(value);
-      };
+    const handleMouseOver = (value) => {setHoverValue(value);};
+    const handleMouseLeave = () => {setHoverValue(0);};
+    const handleClick = (value) => {setSelectedValue(value);};
 
     const { closeModal } = useModal()
     return (
     <form className="create-review-form" >
          <h1 className="review-form-title">How was your stay?</h1>
-         <input type='text' className="review-form-input" placeholder='Leave your review here'></input>
+         <input type='text' 
+                className="review-form-input" 
+                placeholder='Leave your review here'
+                value={reviewContent}
+				onChange={(e) => setReviewContent(e.target.value)}
+                required>
+        </input>
          <div className='review-star-container'>
             <div className="star-rating">
                     {Array.of(1,2,3,4,5).map((_, index) => {
@@ -42,7 +41,7 @@ export default function ReviewFormModal (){
             </div>
             <label><strong> Stars</strong></label>
          </div>
-         <button className="review-submit-button" onClick={closeModal}>Submit Your Review</button>
+         <button className="review-submit-button" onClick={closeModal} disabled={reviewContent.length<10 || !selectedValue}>Submit Your Review</button>
     </form> 
     )
 
